0cf3c9f21a104d4cbe3cf471ab87da9a49e5c966
Anthony Lackey <Anthony Lackey>
2016-04-20 05:04:57 -0700

    Gets command; removes prompt on submit

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,4 @@
-console.log("hello");
+//Listens for when Ctrl+Shift+L is pressed
 chrome.commands.onCommand.addListener(function(command) {
     chrome.tabs.executeScript({
         file: "scripts/prompt.js"
--- scripts/prompt.js
+++ scripts/prompt.js
@@ -1,15 +1,15 @@
-var body = document.getElementsByTagName('body')[0];
+//Create command form
 var form = document.createElement("form");
 var input = document.createElement("input");
 
-form.setAttribute("method", "get");
+form.setAttribute("id", "commandlight_form");
+form.style.zIndex = "2147483638";
 
 input.setAttribute("type", "text");
 input.setAttribute("placeholder", "Enter command...");
 input.setAttribute("id", "commandlight_prompt");
 input.setAttribute("name", "commandlight_prompt");
 input.setAttribute("autofocus", "autofocus");
-
 input.style.position = "absolute";
 input.style.width = "80%";
 input.style.top = "30%";
@@ -17,7 +17,25 @@ input.style.right = "10%";
 input.style.backgroundColor = "gray";
 input.style.color = "white";
 input.style.fontSize = "3em";
+input.style.padding = "2px 2px 2px 2px";
+input.style.border = "0";
+input.style.borderRadius = "3px";
 
 form.appendChild(input);
 
+//Add form to page
+var body = document.getElementsByTagName('body')[0];
 body.appendChild(form);
+
+//Add onsubmit listener
+document.getElementById("commandlight_form").addEventListener("submit", function(event){
+    //Get command
+    var command = document.getElementById("commandlight_prompt").value;
+    alert(command);
+
+    //Remove prompt display
+    var form = document.getElementById("commandlight_form");
+    form.parentNode.removeChild(form);
+
+    event.preventDefault();
+});
